4206a054e3806f3b574f69e11b8abab4c4ccfbaa
vyko-24 <20223tn008@utez.edu.mx>
2025-11-12 19:47:10 -0600

    cosas de certbot pt2

--- client/src/modules/test/test.controller.js
+++ client/src/modules/test/test.controller.js
@@ -1,7 +1,7 @@
 const TestController = {};
 const ENV = import.meta.env;
 
-const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
+const API_URL = `${ENV.VITE_API_PROTOCOL}://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
 
 TestController.callToApi = async () => {
     await fetch(`${API_URL}/test`, {
